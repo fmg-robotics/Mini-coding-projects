014c65b69756d57c9e339057b8dee68cf7f93ca8
fmgRobotics <94498258+fmg-robotics@users.noreply.github.com>
2022-05-02 17:55:39 -0400

    Create motion-tracking.py

---
+++ motion-tracking.py
@@ -0,0 +1,42 @@
+# uses open cv to track an obect you select
+
+import cv2
+
+# video capture
+
+cap = cv2.VideoCapture(0)
+
+# init selection
+
+tracker = cv2.legacy.TrackerCSRT_create()
+success, img = cap.read()
+bbox = cv2.selectROI("Tracking", img, False)
+tracker.init(img, bbox)
+
+
+# defining functions
+
+def drawBox(img, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]),
+    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+
+
+# motion tracking
+
+while True:
+    timer = cv2.getTickCount()
+    success, img = cap.read()
+
+    success, bbox = tracker.update(img)
+    print(bbox)
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 1, 243), 2)
+
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 1, 243), 2)
+    cv2.imshow("Tracking", img)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
